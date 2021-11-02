7f09c7a71ee346879570b9101c0a72dd11fadba1
Ralf S. Engelschall <rse@engelschall.com>
2021-11-02 19:40:47 +0100

    upgrade dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         eslint: {
             options: {
-                configFile: "eslint.yaml"
+                overrideConfigFile: "eslint.yaml"
             },
             "graphql-tools-sequelize": [ "src/**/*.js", "tst/**/*.js" ]
         },
